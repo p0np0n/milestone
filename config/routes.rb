9833daf7698b7e916d6306f6fdcdcc8bdb54dc2e
p0np0n <pond@sennalabs.com>
2020-07-17 16:58:32 +0700

    feat: routing api template

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,16 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: :json} do
+    namespace :rest, defaults: {format: :json} do
+      namespace :v1, defaults: {format: :json} do
+      end
+    end
+
+    namespace :graph, defaults: {format: :json} do
+      namespace :v1, defaults: {format: :json} do
+      end
+    end
+  end
 end
