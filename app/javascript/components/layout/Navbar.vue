16d558811585c67e692b9cb0499fd9e6aeacea00
p0np0n <pond@sennalabs.com>
2020-07-20 17:58:00 +0700

    feat: add vuejs into project

---
+++ app/javascript/components/layout/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <div id="navbarSupportedContent" class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link :to="{ name: 'welcome' }" class="nav-link">
+              Welcome Page
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
---
+++ app/javascript/pages/home/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-center">
+    <h1>Welcome to Milestone</h1>
+    <hr class="invisible" />
+  </div>
+</template>
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
